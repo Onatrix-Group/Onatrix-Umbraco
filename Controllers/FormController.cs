2e4e50c3e60b433e1554e0e6c03073cfe598a0c3
Mona Khorasani <monakhrsn@gmail.com>
2025-11-11 16:36:39 +0100

    Develop formSection, save data, Success and error messages, styling

---
+++ Controllers/FormController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using OnatrixUmbraco.Services;
+using OnatrixUmbraco.ViewModels;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace OnatrixUmbraco.Controllers;
+
+public class FormController(
+    IUmbracoContextAccessor umbracoContextAccessor,
+    IUmbracoDatabaseFactory databaseFactory,
+    ServiceContext services,
+    AppCaches appCaches,
+    IProfilingLogger profilingLogger,
+    IPublishedUrlProvider publishedUrlProvider,
+    FormSubmissionsService formSubmissions)
+    : SurfaceController(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger,
+        publishedUrlProvider)
+{
+    // GET
+
+    private readonly FormSubmissionsService _formSubmissions = formSubmissions;
+
+
+    public IActionResult HandleCallbackForm(CallbackFormViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return CurrentUmbracoPage();
+        }
+        
+        var result = _formSubmissions.SaveCallbackRequest(model);
+        if (!result)
+        {
+            TempData["FormError"] = "Somthing went wrong while submitting you request. PLease try again later";
+            return RedirectToCurrentUmbracoPage();
+        }
+        
+        TempData["FormSuccess"] = "Form submission saved successfully";
+        
+        return RedirectToCurrentUmbracoPage();
+    }
+}
